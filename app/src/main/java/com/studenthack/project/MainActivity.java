5b2628a084bc51dc7f28617c0cdcb2e97593c3e3
Leo <chun.huang@student.manchester.ac.uk>
2019-03-30 14:01:50 +0000

    Code for potentially reading from Bluetooth (doesn't work yet, don't know how to test)

--- app/src/main/java/com/studenthack/project/MainActivity.java
+++ app/src/main/java/com/studenthack/project/MainActivity.java
@@ -2,23 +2,40 @@ package com.studenthack.project;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b1,b2,b3,b4;
     private BluetoothAdapter BA;
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
     private Set<BluetoothDevice> pairedDevices;
+    private final String TARGET_MAC = "";
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    private boolean found = false;
+    TextView textView;
+    Thread thread;
+    byte buffer[];
     ListView lv;
 
     @Override
@@ -33,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         BA = BluetoothAdapter.getDefaultAdapter();
         lv = (ListView)findViewById(R.id.listView);
+        textView = (TextView) findViewById(R.id.textView);
     }
 
     public void on(View v){
@@ -51,9 +69,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public  void visible(View v){
-        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        startActivityForResult(getVisible, 0);
+    public  void listen(View v){
+        beginListenForData();
     }
 
 
@@ -62,11 +79,81 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList list = new ArrayList();
 
-        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
+        for(BluetoothDevice bt : pairedDevices) list.add(bt.getAddress());
         Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
 
         final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
 
         lv.setAdapter(adapter);
+    }
+
+    public void connectDevice(View v) {
+        for(BluetoothDevice bt : pairedDevices)
+            if(bt.getAddress().equals(TARGET_MAC)) //Replace with iterator.getName() if comparing Device names.
+            {
+                device = bt; //device is an object of type BluetoothDevice
+                found=true;
+                break;
+            }
+
+        boolean connected=true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+        if(connected)
+        {
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream=socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        buffer = new byte[1024];
+        Thread thread  = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted())
+                {
+                    try
+                    {
+                        int byteCount = inputStream.available();
+                        if(byteCount > 0)
+                        {
+                            byte[] rawBytes = new byte[byteCount];
+                            inputStream.read(rawBytes);
+                            final String string=new String(rawBytes,"UTF-8");
+                            handler.post(new Runnable() {
+                                public void run()
+                                {
+                                    textView.append(string);
+                                }
+                            });
+
+                        }
+                    }
+                    catch (IOException ex)
+                    { // Do nothing
+                    }
+                }
+            }
+        });
+
+        thread.start();
     }
 }
