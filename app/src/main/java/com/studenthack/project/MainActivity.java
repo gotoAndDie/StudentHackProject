c7f4de2425c842def604834ff46888385059f642
Leo <chun.huang@student.manchester.ac.uk>
2019-03-30 14:09:15 +0000

    Replaced all code with a demo targeted towards sending and receiving data to/from embedded devices

--- app/src/main/java/com/studenthack/project/MainActivity.java
+++ app/src/main/java/com/studenthack/project/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,80 +23,83 @@ import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button b1,b2,b3,b4;
-    private BluetoothAdapter BA;
+    //    private final String DEVICE_NAME="MyBTBee";
+    private final String DEVICE_ADDRESS="20:13:10:15:33:66";
     private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
-    private Set<BluetoothDevice> pairedDevices;
-    private final String TARGET_MAC = "";
     private BluetoothDevice device;
     private BluetoothSocket socket;
     private OutputStream outputStream;
     private InputStream inputStream;
-    private boolean found = false;
+    Button startButton, sendButton,clearButton,stopButton;
     TextView textView;
+    EditText editText;
+    boolean deviceConnected=false;
     Thread thread;
     byte buffer[];
-    ListView lv;
-
+    int bufferPosition;
+    boolean stopThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        b1 = (Button) findViewById(R.id.button);
-        b2=(Button)findViewById(R.id.button2);
-        b3=(Button)findViewById(R.id.button3);
-        b4=(Button)findViewById(R.id.button4);
-
-        BA = BluetoothAdapter.getDefaultAdapter();
-        lv = (ListView)findViewById(R.id.listView);
+        startButton = (Button) findViewById(R.id.buttonStart);
+        sendButton = (Button) findViewById(R.id.buttonSend);
+        clearButton = (Button) findViewById(R.id.buttonClear);
+        stopButton = (Button) findViewById(R.id.buttonStop);
+        editText = (EditText) findViewById(R.id.editText);
         textView = (TextView) findViewById(R.id.textView);
+        setUiEnabled(false);
+
     }
 
-    public void on(View v){
-        if (!BA.isEnabled()) {
-            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(turnOn, 0);
-            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
+    public void setUiEnabled(boolean bool)
+    {
+        startButton.setEnabled(!bool);
+        sendButton.setEnabled(bool);
+        stopButton.setEnabled(bool);
+        textView.setEnabled(bool);
+
+    }
+
+    public boolean BTinit()
+    {
+        boolean found=false;
+        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
         }
-    }
-
-    public void off(View v){
-        BA.disable();
-        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
-    }
-
-
-    public  void listen(View v){
-        beginListenForData();
-    }
-
-
-    public void list(View v){
-        pairedDevices = BA.getBondedDevices();
-
-        ArrayList list = new ArrayList();
-
-        for(BluetoothDevice bt : pairedDevices) list.add(bt.getAddress());
-        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
-
-        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
-
-        lv.setAdapter(adapter);
-    }
-
-    public void connectDevice(View v) {
-        for(BluetoothDevice bt : pairedDevices)
-            if(bt.getAddress().equals(TARGET_MAC)) //Replace with iterator.getName() if comparing Device names.
-            {
-                device = bt; //device is an object of type BluetoothDevice
-                found=true;
-                break;
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter, 0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+        if(bondedDevices.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for (BluetoothDevice iterator : bondedDevices)
+            {
+                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+                {
+                    device=iterator;
+                    found=true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
 
+    public boolean BTconnect()
+    {
         boolean connected=true;
         try {
             socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
@@ -118,17 +122,35 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+
+
+        return connected;
+    }
+
+    public void onClickStart(View view) {
+        if(BTinit())
+        {
+            if(BTconnect())
+            {
+                setUiEnabled(true);
+                deviceConnected=true;
+                beginListenForData();
+                textView.append("\nConnection Opened!\n");
+            }
+
+        }
     }
 
     void beginListenForData()
     {
         final Handler handler = new Handler();
+        stopThread = false;
         buffer = new byte[1024];
         Thread thread  = new Thread(new Runnable()
         {
             public void run()
             {
-                while(!Thread.currentThread().isInterrupted())
+                while(!Thread.currentThread().isInterrupted() && !stopThread)
                 {
                     try
                     {
@@ -148,12 +170,39 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                     catch (IOException ex)
-                    { // Do nothing
+                    {
+                        stopThread = true;
                     }
                 }
             }
         });
 
         thread.start();
+    }
+
+    public void onClickSend(View view) {
+        String string = editText.getText().toString();
+        string.concat("\n");
+        try {
+            outputStream.write(string.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        textView.append("\nSent Data:"+string+"\n");
+
+    }
+
+    public void onClickStop(View view) throws IOException {
+        stopThread = true;
+        outputStream.close();
+        inputStream.close();
+        socket.close();
+        setUiEnabled(false);
+        deviceConnected=false;
+        textView.append("\nConnection Closed!\n");
+    }
+
+    public void onClickClear(View view) {
+        textView.setText("");
     }
 }
